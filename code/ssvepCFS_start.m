e942dfe7a2ed53f99687fd45e21d692652fee336
ManuelMejia <maalmer@gmail.com>
2024-04-01 13:41:17 -0700

    Added block repetition

--- code/ssvepCFS_start.m
+++ code/ssvepCFS_start.m
@@ -1,11 +1,15 @@
 
 
 
-
-
 % [data, expmnt] = ssvepCFS(subject, suppression, supp_alpha);
 % suppression: 0=visible,  1=suprimido
 % supp_alpha:  anotar numero de curva psicofisica
 
-[data, expmnt] = ssvepCFS('ManuelMejia', 1, 0.15)
+
+
+% Knowing alpha blending values:
+[data, expmnt] = ssvepCFS('ManuelMejia', [0.04, 0.32])
+
+% Not knowing alpha blending values:
+[data, expmnt] = ssvepCFS('ManuelMejia')
 
