e1a2285d0bc756ce7d7ff553616392f1ac723e68
ManuelMejia <maalmer@gmail.com>
2024-07-07 17:34:56 -0700

    Cleaned block conditions (only 4 blocks), changed phot selection to the beginning

--- code/ssvepCFS_start.m
+++ code/ssvepCFS_start.m
@@ -2,29 +2,20 @@
 %% Debug: -----------------------------------------------------------------
 % Usar estas dos lineas para 'probar' el codigo. Desactiva el trigger al
 % BrainVission, y muestra la pantalla transparente.
-% Estimation of threshold
-[data, expmnt] = ssvepCFS('NombreParticipante', 'pf', 1, 'debug',1, 'triggers',0);
-
+% Threshold estimation
+[data, expmnt] = ssvepCFS('NombreParticipante', 'pf',1, 'debug',1, 'triggers',0);
 % Main task
-[data, expmnt] = ssvepCFS('NombreParticipante', 'alphas', [0.03,0.30], 'debug',1, 'triggers',0);
-
-
+[data, expmnt] = ssvepCFS('NombreParticipante', 'debug',1, 'triggers',0);
 
 %% Run: -------------------------------------------------------------------
-% Usar estos codigos para ejecutar la tarea.
 % 1-Primero se estima el umbral para el participante ('pf', 1).
-% 2-Luego se calculan los valores para el alpha blending 'calculate_alpha'
-% 3-Y por ultimo, se ejecuta la tarea principal completa: pero agregar los
-% valores que den para el alpha (o usar los default para probar).
+% 2-Segundo, se ejecuta la tarea principal completa.
 %% Task 1: Psychophysics task to estimate alpha blending values:
-% Stereograms En orden: 1=A; 2=C; 3=H; 4=P; 5=L; 6=S; 7=T; 8=X; 9=Z;
-[data, expmnt] = ssvepCFS('NombreParticipante', 'pf', 1, 'sim', 1);
- 
-%% Task 2: Knowing alpha blending values:
-% Stereograms En orden: 1=A; 2=C; 3=H; 4=P; 5=L; 6=S; 7=T; 8=X; 9=Z;
-% 2=Upright, 3=Inverted
+% Stereograms En orden: 1=A; 2=C; 3=H; 4=J;
+[data, expmnt] = ssvepCFS('NombreParticipante1', 'pf',1);
+
+%% Task 2: After estimation of alpha blending values:
+% Stereograms En orden: 1=L; 2=O; 3=P; 4=S; 5=T; 6=X; 7=Z;
 ssvepCFS('NombreParticipante', 'calculate_alpha');
-[data, expmnt] = ssvepCFS('NombreParticipante', 'blockstart', 1, 'sim', 1);
-
-
+[data, expmnt] = ssvepCFS('NombreParticipante1', 'blockstart',1);
 
